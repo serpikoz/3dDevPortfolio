daca54052bf5bde108eba0a1038f8f55b9c2b1b0
Kadir <116947551+serpikoz@users.noreply.github.com>
2023-11-23 16:17:53 +0300

    final

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -7,6 +7,10 @@ import { EarthCanvas } from "./canvas";
 import { SectionWrapper } from "../hoc";
 import { slideIn } from "../utils/motion";
 
+// // template_1fcd4xi
+// service_8glx1qq
+// Aa4LwMRK9ZHuTHgkA
+
 const Contact = () => {
   const formRef = useRef();
   const [form, setForm] = useState({
@@ -33,16 +37,16 @@ const Contact = () => {
 
     emailjs
       .send(
-        import.meta.env.VITE_APP_EMAILJS_SERVICE_ID,
-        import.meta.env.VITE_APP_EMAILJS_TEMPLATE_ID,
+        "service_8glx1qq",
+        "template_12wfqkn",
         {
           from_name: form.name,
-          to_name: "JavaScript Mastery",
+          to_name: "kadir",
           from_email: form.email,
-          to_email: "sujata@jsmastery.pro",
+          to_email: "kadir-ozyurek@outlook.com",
           message: form.message,
         },
-        import.meta.env.VITE_APP_EMAILJS_PUBLIC_KEY
+        "Aa4LwMRK9ZHuTHgkA"
       )
       .then(
         () => {
